8e9d8d4c5d98bc0063ce75ad6234ad2db3991136
Dollyg <dollyg@thoughtworks.com>
2020-04-19 01:49:47 +0530

    bootstrap

---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Pokemon() {
+    return (
+        <img src="https://pokeres.bastionbot.org/images/pokemon/1.png" alt="logo" />
+    )
+}
+
+export default Pokemon
